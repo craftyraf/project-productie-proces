84af7b2ba2a97789177ce732d3d05aef126f5901
RafMesotten <rafmesotten@gmail,com>
2024-04-01 19:36:01 +0200

    Voorzet gedaan voor Deel 1 vragen 6 en 7. Wat nog zeker moet gebeuren (en waar ik nog geen oplossing voor heb): automatiseren van inputvariabelen van de simuleer functies.

---
+++ scripts/simulate.py
@@ -0,0 +1,18 @@
+import random
+import numpy as np
+
+def simulate(share_1, share_2, lower_bound, upper_bound, mean, stdev, num_values, n_days):
+    all_random_sums = []
+    for _ in range(num_values):
+        random_sum = 0
+        for _ in range(n_days):
+            random_number = random.random()
+            if random_number < share_1 / 100:
+                random_value = 0
+            elif random_number < (share_1 + share_2) / 100:
+                random_value = np.random.uniform(lower_bound, upper_bound)
+            else:
+                random_value = np.random.normal(mean, stdev)
+            random_sum += random_value
+        all_random_sums.append(random_sum)
+    return all_random_sums
