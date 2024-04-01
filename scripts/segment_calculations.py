454a7805b79721fd9dc14ed26a9ed782d92bcce3
RafMesotten <rafmesotten@gmail,com>
2024-04-01 15:02:08 +0200

    Deel 1 vraag 5 laatste keer bijgewerkt. Print segment share in aparte functie gestoken, want deze code stond dubbel bij BRU en STO.

--- scripts/segment_calculations.py
+++ scripts/segment_calculations.py
@@ -38,3 +38,23 @@ def create_segments(location, threshold_1, threshold_2, df):
     share_3 = round((len(segment_3) * 100) / total_days_location, 2)
 
     return share_1, share_2, share_3, segment_1, segment_2, segment_3
+
+
+def print_segment_share(location, threshold_1, threshold_2, share_1, share_2, share_3):
+    """
+    Print the share of days per segment.
+
+    Parameters:
+        location (str): The location name.
+        threshold_1 (float): The first threshold value.
+        threshold_2 (float): The second threshold value.
+        share_1 (float): The share of days with production < threshold_1.
+        share_2 (float): The share of days with threshold_1 <= production < threshold_2.
+        share_3 (float): The share of days with production >= threshold_2.
+
+    Returns:
+        None
+    """
+    print(f"{location}: % dagen met [productie < {threshold_1}]:", share_1, '%')
+    print(f"{location}: % dagen met [{threshold_1} <= productie < {threshold_2}]:", share_2, '%')
+    print(f"{location}: % dagen met [productie >= {threshold_2}]:", share_3, '%')
